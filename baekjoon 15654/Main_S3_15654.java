74cfd58ae8bcd94f94d7672f24d59afff94bea68
postkim03 <postkim03@naver.com>
2021-09-20 22:32:55 +0900

    to 0920

---
+++ baekjoon 15654/Main_S3_15654.java
@@ -0,0 +1,54 @@
+package BOJ_silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_S3_15654 {
+	static int[] numbers;
+	static int[] input;
+	static boolean[] visited;
+	static int n;
+	static int m;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		numbers = new int[m];
+		input = new int[n + 1];
+		visited = new boolean[n + 1];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(input);
+		
+		perm(0);
+		System.out.println(sb);
+	}
+	
+	public static void perm(int cnt) {
+		if (cnt == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(numbers[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = 1; i <= n; i++) {
+			if (visited[i]) continue;
+			numbers[cnt] = input[i];
+			visited[i] = true;
+			perm(cnt + 1);
+			visited[i] = false;
+		}
+	}
+}
---
+++ baekjoon 15657/Main_S3_15657.java
@@ -0,0 +1,49 @@
+package BOJ_silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_S3_15657 {
+	static int[] numbers;
+	static int[] input;
+	static int n;
+	static int m;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		numbers = new int[m];
+		input = new int[n + 1];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(input);
+		
+		comb(0, 1);
+		System.out.println(sb);
+	}
+	
+	public static void comb(int cnt, int start) {
+		if (cnt == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(numbers[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = start; i <= n; i++) {
+			numbers[cnt] = input[i];
+			comb(cnt + 1, i);
+		}
+	}
+}
