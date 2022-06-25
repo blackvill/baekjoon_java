276b811a7f4df835624cbd184c1ede6a0ce9a28c
postkim03 <postkim03@naver.com>
2022-06-25 16:24:46 +0900

    to 0625

---
+++ baekjoon 25083/Main_B5_25083.java
@@ -0,0 +1,15 @@
+package BOJ_bronze;
+
+import java.io.*;
+
+public class Main_B5_25083 {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
