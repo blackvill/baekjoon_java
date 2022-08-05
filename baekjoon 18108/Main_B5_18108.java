4a2cc33426e9b621f3c57ddde070c44b68d52894
postkim03 <postkim03@naver.com>
2022-08-05 22:06:13 +0900

    to 0805

---
+++ baekjoon 18108/Main_B5_18108.java
@@ -0,0 +1,15 @@
+package BOJ_bronze;
+
+import java.io.*;
+
+public class Main_B5_18108 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = 2541 - 1998;
+		int x = Integer.parseInt(br.readLine());
+		
+		System.out.println(x - n);
+	}
+}
