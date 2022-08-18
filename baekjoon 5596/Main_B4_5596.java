f23d73948c6d18b87b829d96a912526cca8e59ad
postkim03 <postkim03@naver.com>
2022-08-18 22:18:52 +0900

    to 0818

---
+++ baekjoon 5596/Main_B4_5596.java
@@ -0,0 +1,30 @@
+package BOJ_bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_B4_5596 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int s = 0;
+		int t = 0;
+		
+		for (int i = 0; i < 4; i++) {
+			s += Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < 4; i++) {
+			t += Integer.parseInt(st.nextToken());
+		}
+		
+		if (s >= t)
+			System.out.println(s);
+		else
+			System.out.println(t);
+		
+	}
+}
